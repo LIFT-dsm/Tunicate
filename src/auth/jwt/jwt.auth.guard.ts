0955efa6a33b50d5b5b541e25fcdf4d8b334e3f2
오송주 <dhthdwn7920@gmail.com>
2024-09-03 19:53:01 +0900

    refector:: user와 auth 분리 및 메소드 분리

--- src/auth/jwt/jwt.auth.guard.ts
+++ src/auth/jwt/jwt.auth.guard.ts
@@ -11,19 +11,27 @@ export class JwtAuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest();
-
     const token: string = req.headers['Authorization'];
 
-    if (!token) throw new BadRequestException('토큰이 필요합니다');
-    if (!token.startsWith(process.env.JWT_PREFIX)) throw new BadRequestException('토큰형식 오류');
+    this.checkToken(token);
 
     const { id } = await this.jwt.decode(token.split(' ')[1]);
+    req.body.user = await this.findUser(id);
 
+    return true;
+  }
+
+  // token 유효성 검사
+  private checkToken(token: string) {
+    if (!token) throw new BadRequestException('토큰이 필요합니다');
+    if (!token.startsWith(process.env.JWT_PREFIX)) throw new BadRequestException('토큰형식 오류');
+  }
+
+  // 유저 검색
+  private async findUser(id: number) {
     const user = await this.userRepository.findOneUserByStudentId(id);
     if (!user) throw new NotFoundException('존재하지 않는 유저');
 
-    req.body.user = user;
-
-    return true;
+    return user;
   }
 }
