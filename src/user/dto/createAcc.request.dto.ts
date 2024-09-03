ec9cad84e406930bcede3c97f3cd0754d25580ae
오송주 <dhthdwn7920@gmail.com>
2024-09-03 14:50:30 +0900

    feat: create signup function

---
+++ src/user/dto/createAcc.request.dto.ts
@@ -0,0 +1,19 @@
+import { IsNumber, IsString } from 'class-validator';
+import { Gender } from '../entities/user.entities';
+
+export class CreateAccRequestDto {
+  @IsNumber()
+  studentId: number;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  nickname: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  gender: Gender;
+}
