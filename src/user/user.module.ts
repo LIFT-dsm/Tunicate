0f0e7dedf2fbbbd65aa6df331b0a4486ca7d3a36
오송주 <dhthdwn7920@gmail.com>
2024-09-03 18:56:19 +0900

    fix:: solve conflict

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,22 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entities';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService, Logger],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '3h',
+      },
+      verifyOptions: {
+        complete: false,
+      },
+    }),
+  ],
+  providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
 })
