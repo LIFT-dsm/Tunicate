8ece21c90cfda08a50e52617a8bafafd680378fc
101 <tmddnjs001133@dsm.hs.kr>
2024-09-19 19:49:12 +0900

    add: open service

---
+++ src/party/servises/party.open.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@nestjs/common";
+import { PartyRepository } from "../repositories/party.repository";
+import { PartyOpenDto } from "../dto/party-open.dto";
+import { Override } from "src/utils/decorators/override.decorator";
+import { PartyOpenUseCase } from "../usecase/party.open.usecase";
+
+@Injectable()
+export class PartyOpenService implements PartyOpenUseCase {
+    constructor(
+        private readonly repository: PartyRepository
+    ) {
+    }
+
+    @Override()
+    async open(dto: PartyOpenDto) {
+        const party = {
+            name: dto.partyName,
+            destination: dto.destination,
+            startDate: dto.startDate,
+            endDate: dto.endDate,
+            code: this.generateRandomPartyCode(),
+        };
+
+        await this.repository.save(party);
+    }
+
+    private generateRandomPartyCode(): string {
+        return Math.random().toString().slice(2, 8);
+    }
+}
