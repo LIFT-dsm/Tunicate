efe6253f3b4c92659098c722fa894db360759206
101 <tmddnjs001133@dsm.hs.kr>
2024-09-19 19:49:23 +0900

    update: 의존성 관리

--- src/party/party.module.ts
+++ src/party/party.module.ts
@@ -1,18 +1,34 @@
 import { Module } from "@nestjs/common";
 import { Party } from "./entities/party.entities";
-import { PartyService } from "./party.service";
-import { PartyController } from "./party.controller";
+import { PartyController } from "./controllers/party.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { PartyRepositoryInterface } from "./repositories/party.repository.interface";
 import { PartyRepository } from "./repositories/party.repository";
+import { PartyRepositoryImpl } from "./repositories/party.repository.impl";
+import { PartyOpenUseCase } from "./usecase/party.open.usecase";
+import { PartyOpenService } from "./servises/party.open.service";
+import { PartyCancelUseCase } from "./usecase/party.cancel.usecase";
+import { PartyCancelService } from "./servises/party.cancel.service";
+import { PartyUpdateUseCase } from "./usecase/party.update.usecase";
+import { PartyUpdateService } from "./servises/party.update.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Party])],
     providers: [
-        PartyService,
         {
-            provide: PartyRepositoryInterface,
-            useClass: PartyRepository
+            provide: PartyOpenUseCase,
+            useClass: PartyOpenService
+        },
+        {
+            provide: PartyCancelUseCase,
+            useClass: PartyCancelService
+        },
+        {
+            provide: PartyUpdateUseCase,
+            useClass: PartyUpdateService
+        },
+        {
+            provide: PartyRepository,
+            useClass: PartyRepositoryImpl
         }
     ],
     controllers: [PartyController],
