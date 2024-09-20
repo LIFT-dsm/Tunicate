954adc7cd0ae3d478a764702afb73ffee422ef91
101 <tmddnjs001133@dsm.hs.kr>
2024-09-20 16:36:03 +0900

    add: 인증 로직 추가

--- src/party/servises/party.update.service.ts
+++ src/party/servises/party.update.service.ts
@@ -1,7 +1,7 @@
 import { Override } from "src/utils/decorators/override.decorator";
 import { PartyRepository } from "../repositories/party.repository";
 import { PartyUpdateDto } from "../dto/party-update.dto";
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class PartyUpdateService {
@@ -12,7 +12,13 @@ export class PartyUpdateService {
 
     @Override()
     async update(id: number, dto: PartyUpdateDto) {
+        const currentUser = dto.user;
         const party = await this.repository.findById(id);
+
+        if (currentUser.studentId !== party.leader.studentId) {
+            throw new ForbiddenException('해당 파티를 수정할 수 없습니다.');
+        }
+
         Object.assign(party, dto);
 
         await this.repository.update(party);
